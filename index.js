96919ae0126b3cd20b0c77671570333380d3bf02
coumbaaziz <coumbaazizka9gmail.com>
2023-07-05 17:29:20 +0000

    html

---
+++ index.js
@@ -0,0 +1,9 @@
+let inscrire = document.querySelector(".inscrire")
+let connecter = document.querySelector(".Connecter")
+
+let inscrireBtn = document.querySelector(".inscrireBtn")
+let connecterBtn = document.querySelector(".ConnecterBtn")
+
+inscrireBtn.addEventListener("click", () => {
+    inscrire.classList.remove("hidden")
+})
